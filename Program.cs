3ed26d932fb9f980a2023a746ac38cc179f3bf0a
Ehsan Jalali <JMSEhsan@gmail.com>
2021-11-27 00:03:29 -0700

    Commit: developed

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ class Program
         public string City {get; set;}
     }
 
-
     static void Main(string[] args)
     {
         Person [] people = new Person []
@@ -22,14 +21,25 @@ class Program
         
             new Person {Name = "Alen", City = "Hull"},
             new Person {Name = "Beryl", City = "Seattle"},
-            new Person {Name = "Charles", City = "London"}
+            new Person {Name = "Charles", City = "London"},
+            new Person {Name = "David", City = "Seattle"},
+            new Person {Name = "Eddy", City = "Paris"},
+            new Person {Name = "Fred", City = "Berlin"},
+            new Person {Name = "Gordon", City = "Hull"},
+            new Person {Name = "Henry", City = "Seattle"},
+            new Person {Name = "Isaac", City = "Seattle"},
+            new Person {Name = "James", City = "London"}
         };
 
-            var result = from person in people.AsParallel()
-            where person.City == "Seattle" select person;
-        
+        var result = from person in people.AsParallel()
+        where person.City == "Seattle" select person;
 
+        foreach (var person in result)
+        Console.WriteLine(person.Name);     
+
+
+        Console.WriteLine("Finished processing. Press a key to end.");
+        Console.ReadKey();
     }
-    
 }
 
